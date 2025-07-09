96b2388a6ff3f67c4128b51ec6257c4e61ffa363
Jonathan Villegas <130876405+joribavi@users.noreply.github.com>
2025-07-09 18:33:45 -0300

    Update prophets.js
    Editing on Git after delay with Visual Studio Code

--- scripts/prophets.js
+++ scripts/prophets.js
@@ -12,21 +12,30 @@ getProphetData();
 
 
 const displayProphets = (prophets) => {
-    prophets.forEach(prophet => {
+    prophets.forEach((prophet) => {
 
         let card = document.createElement('section');
+        card.classList.add('prophet-card');
         let fullName = document.createElement('h2');
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
         let portrait = document.createElement('img');
 
         fullName.textContent = `${prophet.name} ${prophet.lastname}`;
-        portrait.setAttribute('scr', prophet.imageurl);
+        birthDate.textContent = `Date of birth: ${prophet.birthdate}`;
+        birthPlace.textContent = `Place of birth: ${prophet.birthplace}`;
+        
+        portrait.setAttribute('src', prophet.imageurl);
         portrait.setAttribute('alt', `Portrait of ${prophet.lastname}`);
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
 
-        card.appendChild(cards);
+        card.appendChild(fullName);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
         card.appendChild(portrait);
+
 
         cards.appendChild(card);
 

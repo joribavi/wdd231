a109dcf6cc6522ccc1ded2ebf9dd1da5c146d3ef
Jonathan Villegas <jvillegas86@gmail.com>
2025-07-11 00:02:05 -0300

    working on w2 assignment

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,26 @@
+// Footer Current Year and Last Modified
+const currentyear = document.querySelector("#currentyear");
+
+// use the date object
+const today = new Date();
+currentyear.innerHTML = ` <span class="highlight">${today.getFullYear()}</span> `;
+
+
+let lastmodified = new Date(document.lastModified);
+document.getElementById('lastModified').textContent = lastmodified.toLocaleString();
+
+//navigation menu
+
+const navbutton = document.querySelector("#ham-btn");
+
+const navBar = document.querySelector('#nav-bar');
+
+//toggle the show class off and on
+
+navbutton.addEventListener('click', () => {
+    navbutton.classList.toggle('show');
+    navBar.classList.toggle('show');
+
+});
+
+

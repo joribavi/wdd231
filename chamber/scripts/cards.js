66d65c9f4944553ae3ed0a99f31a70cb619dd16d
Jonathan Villegas <jvillegas86@gmail.com>
2025-07-18 16:13:57 -0300

    ending functions for week03 assignment

--- chamber/scripts/cards.js
+++ chamber/scripts/cards.js
@@ -2,6 +2,11 @@
 const directory = 'data/business.json';
 const directoryCards = document.querySelector('.businesses');
 
+
+
+
+
+
 async function getCardData() {
     const response = await fetch(directory)
     const data = await response.json();
@@ -13,7 +18,17 @@ async function getCardData() {
 getCardData()
 
 const displayCards = (businesses) => {
-    businesses.forEach((business) => {
+
+    // filtering information according to membership level
+    const goldSilverMembers = businesses.filter(business => business.membership_levels.name === 'gold' || business.membership_levels.name === 'silver');
+    //Asking for random data from goldSilverMembers
+    goldSilverMembers.sort(function () {
+        return Math.random() - 0.5;
+    });
+    const twoCards = goldSilverMembers.slice(0, Math.random() > 0.5 ? 3 : 2); // inside this Math.random: Math.random() > 0.5 returns true or false randomly( if true returns 3, if false returns 2)
+
+
+    twoCards.forEach((business) => {
 
         let card = document.createElement('section');
         card.classList.add('directory-card');
@@ -21,10 +36,13 @@ const displayCards = (businesses) => {
         let addresses = document.createElement('p');
         let webSiteUrl = document.createElement('p');
         let image = document.createElement('img');
+        let membershipLevel = document.createElement('p');
+        membershipLevel.classList.add('membership-level'); //adding class to style membership level Tag
 
         name.textContent = `${business.name} `;
         addresses.textContent = `Addresses: ${business.addresses}`;
         webSiteUrl.textContent = `Website: ${business.website_url}`;
+        membershipLevel.textContent = `${business.membership_levels.name}`;
 
         image.setAttribute('src', `images/${business.image}`);
         image.setAttribute('alt', `Image of ${business.image}`);
@@ -35,7 +53,9 @@ const displayCards = (businesses) => {
         card.appendChild(name);
         card.appendChild(addresses);
         card.appendChild(webSiteUrl);
+        card.appendChild(membershipLevel);
         card.appendChild(image);
+
 
 
         directoryCards.appendChild(card);

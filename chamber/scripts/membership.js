e54eb2bc9c25683cce3462b091acce9fa02c3b5a
Jonathan Villegas <jvillegas86@gmail.com>
2025-07-26 16:53:08 -0300

    ending modal dialog styles

--- chamber/scripts/membership.js
+++ chamber/scripts/membership.js
@@ -1,6 +1,6 @@
 // Modal Dialog for NP Card
 
-const npModal = document.getElementById('#np-membership-modal')
+const npModal = document.getElementById('np-membership-modal')
 const openNpBtn = document.querySelector('#np')
 const closeNpBtn = document.querySelector('#np-closebtn')
 
@@ -11,3 +11,53 @@ openNpBtn.addEventListener("click", () => {
 closeNpBtn.addEventListener("click", () => {
     npModal.close();
 });
+
+
+// Modal Dialog for Bronze Card
+
+const bronzeModal = document.getElementById('bronze-membership-modal')
+const openBronzeBtn = document.querySelector('#bronze')
+const closeBronzeBtn = document.querySelector('#bronze-closebtn')
+
+openBronzeBtn.addEventListener("click", () => {
+    bronzeModal.showModal();
+});
+
+closeBronzeBtn.addEventListener("click", () => {
+    bronzeModal.close();
+});
+
+
+// Modal Dialog for Silver Card
+
+const silverModal = document.getElementById('silver-membership-modal')
+const openSilverBtn = document.querySelector('#silver')
+const closeSilverBtn = document.querySelector('#silver-closebtn')
+
+openSilverBtn.addEventListener("click", () => {
+    silverModal.showModal();
+});
+
+closeSilverBtn.addEventListener("click", () => {
+    silverModal.close();
+});
+
+
+// Modal Dialog for Gold Card
+
+const goldModal = document.getElementById('gold-membership-modal')
+const openGoldBtn = document.querySelector('#gold')
+const closeGoldBtn = document.querySelector('#gold-closebtn')
+
+openGoldBtn.addEventListener("click", () => {
+    goldModal.showModal();
+});
+
+closeGoldBtn.addEventListener("click", () => {
+    goldModal.close();
+});
+
+
+
+
+

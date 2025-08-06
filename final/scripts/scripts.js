4f39ce55a7ef6b03440664432ddfbec7b0cb17a8
Jonathan Villegas <jvillegas86@gmail.com>
2025-08-05 23:20:59 -0300

    working on week06 project assignment

--- final/scripts/scripts.js
+++ final/scripts/scripts.js
@@ -25,31 +25,17 @@ navbutton.addEventListener('click', () => {
 
 //slideshow
 
-let slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+import { plusSlides, currentSlide, showSlides } from './slide.mjs';
 
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+
+document.querySelector('.next').addEventListener('click', () => plusSlides(1));
+document.querySelector('.prev').addEventListener('click', () => plusSlides(-1));
+
+// Para los dots (puntos indicadores)
+document.querySelectorAll('.dot').forEach((dot, index) => {
+    dot.addEventListener('click', () => currentSlide(index + 1));
+});
+
+

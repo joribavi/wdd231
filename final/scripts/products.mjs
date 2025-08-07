2b23587cc687563117677c3cf4af655bfa0e61b7
Jonathan Villegas <jvillegas86@gmail.com>
2025-08-06 23:16:54 -0300

    working on json file

---
+++ final/scripts/products.mjs
@@ -0,0 +1,2 @@
+export const shopProducts = 'data/products.json';
+
--- final/scripts/scripts.js
+++ final/scripts/scripts.js
@@ -39,3 +39,95 @@ document.querySelectorAll('.dot').forEach((dot, index) => {
 });
 
 
+
+//Modal Dialog Index 
+
+//FIRST BUTTON
+
+const box1Modal = document.getElementById('company-info-modal-1')
+const openBox1Btn = document.querySelector('#box-1')
+const closeBox1Btn = document.querySelector('#close-btn-1')
+
+openBox1Btn.addEventListener("click", () => {
+    box1Modal.showModal();
+});
+
+closeBox1Btn.addEventListener("click", () => {
+    box1Modal.close();
+});
+
+//SECOND BUTTON
+const box2Modal = document.getElementById('company-info-modal-2')
+const openBox2Btn = document.querySelector('#box-2')
+const closeBox2Btn = document.querySelector('#close-btn-2')
+
+openBox2Btn.addEventListener("click", () => {
+    box2Modal.showModal();
+});
+
+closeBox2Btn.addEventListener("click", () => {
+    box2Modal.close();
+});
+
+
+
+
+//THIRD BUTTON
+
+const box3Modal = document.getElementById('company-info-modal-3')
+const openBox3Btn = document.querySelector('#box-3')
+const closeBox3Btn = document.querySelector('#close-btn-3')
+
+openBox3Btn.addEventListener("click", () => {
+    box3Modal.showModal();
+});
+
+closeBox3Btn.addEventListener("click", () => {
+    box3Modal.close();
+});
+
+
+//PRODUCT CARDS 
+
+import { shopProducts } from './products.mjs';
+
+const productCards = document.querySelector('.cards-wrapper');
+async function getProductData() {
+    const response = await fetch(shopProducts)
+    const data = await response.json();
+    displayProductCards(data.products)
+
+}
+
+getProductData()
+
+const displayProductCards = (products) => {
+
+    products.forEach(product => {
+        //creating card elements 
+        const productCardsHTML = document.createElement('div')
+        productCardsHTML.classList.add('product-card')
+        const productCardTitle = document.createElement('h3')
+        const productImg = document.createElement('img')
+        const productCardButton = document.createElement('button')
+
+        //adding content to the cards 
+
+
+        productCardTitle.textContent = `${product.name}`
+        productImg.setAttribute('src', `images/${product.image}`)
+        productImg.setAttribute('loading', 'lazy')
+        productImg.setAttribute('alt', `${product.type}`)
+        productCardButton.textContent = `Buy`
+
+        //appending elements to its parent elements
+
+        productCards.appendChild(productCardsHTML)
+        productCardsHTML.appendChild(productCardTitle)
+        productCardsHTML.appendChild(productImg)
+        productCardsHTML.appendChild(productCardButton)
+
+
+    });
+
+}

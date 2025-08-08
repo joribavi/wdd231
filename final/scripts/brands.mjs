ca1d6269ceafdbc343d9325a66873cdd05fe9dce
Jonathan Villegas <jvillegas86@gmail.com>
2025-08-08 17:53:18 -0300

    working on index

---
+++ final/scripts/brands.mjs
@@ -0,0 +1,28 @@
+const brands =
+    [
+        {
+            brandTitle: "Ositos",
+            image: "ositos.webp"
+        },
+        {
+            brandTitle: "Kids",
+            image: "kids.webp"
+        },
+        {
+            brandTitle: "Boys",
+            image: "boys.webp"
+        },
+        {
+            brandTitle: "Girls",
+            image: "girls.webp"
+        },
+        {
+            brandTitle: "Animalitos",
+            image: "animalitos.webp"
+        }
+
+    ];
+
+
+
+export default brands;
---
+++ final/scripts/payment.mjs
@@ -0,0 +1,28 @@
+const paymentMethods =
+    [
+        {
+            paymentTitle: "Wire Transfer",
+            image: "bank.webp"
+        },
+        {
+            paymentTitle: "Credit Card",
+            image: "card.webp"
+        },
+        {
+            PaymentTitle: "Paypal",
+            image: "paypal.webp"
+        },
+        {
+            PaymentTitle: "Google Pay",
+            image: "googlepay.webp"
+        },
+        {
+            PaymentTitle: "Apple Pay",
+            image: "applepay.webp"
+        }
+
+    ];
+
+
+
+export default paymentMethods;
